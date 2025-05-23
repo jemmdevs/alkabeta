2884a48c382aa4da4fc93d422017d175815130f7
jemmdevs <jemmdev@gmail.com>
2025-05-23 09:11:03 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <MainLayout>
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6">Publicaciones recientes</h1>
+        <h1 className="text-2xl font-bold mb-6">Explora!</h1>
         <Posts />
       </div>
     </MainLayout>
