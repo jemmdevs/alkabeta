1ecfd909e0150aa5e7ee2e4a249a86253544b17d
jemmdevs <jemmdev@gmail.com>
2025-05-23 09:26:24 +0200

    deploy  testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextjs from 'eslint-config-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = {
+  extends: ['next/core-web-vitals']
+};
 
-export default eslintConfig;
+export default [
+  {
+    ...nextjs
+  }
+];
